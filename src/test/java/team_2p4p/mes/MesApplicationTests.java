8f1e8ae56701f2195f6febb68d7060f3486f8f62
jeong <deas124@gmail.com>
2023-05-10 09:30:30 +0900

    Initial commit

---
+++ src/test/java/team_2p4p/mes/MesApplicationTests.java
@@ -0,0 +1,13 @@
+package team_2p4p.mes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
