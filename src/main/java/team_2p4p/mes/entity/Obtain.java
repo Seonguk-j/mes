130056f149acd35b34c46bbf478f1e3bd9f3470a
Tae0vv <101691583+Tae0vv@users.noreply.github.com>
2023-05-16 13:50:39 +0900

    의존성 주입 추가 및 패키지설정 ,Obtain Entity

---
+++ src/main/java/team_2p4p/mes/entity/Obtain.java
@@ -0,0 +1,48 @@
+package team_2p4p.mes.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Obtain {
+
+    @Id
+    private Long obtain_id;
+
+    @Column(nullable = false)
+    private long item_id;
+
+    @Column(nullable = false)
+    private long customer_id;
+
+    @Column(nullable = false)
+    private long obtain_amount;
+
+    @Column(nullable = false)
+    private LocalDateTime obtain_date;
+
+    @Column
+    private LocalDateTime customer_request_date;
+
+    @Column
+    private LocalDateTime expect_date;
+
+    @Column(nullable = false)
+    private boolean obtain_stat;
+
+    @Column
+    private LocalDateTime obtain_stat_date;
+
+
+
+
+}
