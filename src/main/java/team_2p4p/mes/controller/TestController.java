06715868befc363ce9456b416f1cac47ab0319e1
Eunseo <supreme327@naver.com>
2023-05-22 14:04:38 +0900

    은서수정

--- src/main/java/team_2p4p/mes/controller/TestController.java
+++ src/main/java/team_2p4p/mes/controller/TestController.java
@@ -1,6 +1,10 @@
 package team_2p4p.mes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +12,7 @@ import team_2p4p.mes.entity.Item;
 import team_2p4p.mes.repository.ItemRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("juicyfreash/")
@@ -20,8 +25,11 @@ public class TestController {
     @GetMapping("test")
     public List<Item> test(){
 
-        List<Item> list = itemRepository.findAll();
-        return list;
+        Pageable pageable = PageRequest.of(0,50);
+
+        Page<Item> all = itemRepository.findAll(pageable);
+
+        return all.get().collect(Collectors.toList());
     }
 
 }
