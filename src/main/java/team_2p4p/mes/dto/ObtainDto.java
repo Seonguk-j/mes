c329ac60fc198adf5fffea41211b8a50cc5695e2
Tae0vv <101691583+Tae0vv@users.noreply.github.com>
2023-05-24 17:28:00 +0900

    Update ObtainDto.java

--- src/main/java/team_2p4p/mes/dto/ObtainDto.java
+++ src/main/java/team_2p4p/mes/dto/ObtainDto.java
@@ -1,24 +1,22 @@
 package team_2p4p.mes.dto;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-
 import java.time.LocalDateTime;
 
-//게시글 요청(Request) 클래스
-@Getter @Setter
-public class ObtainDto {
+@Getter
+@Setter
+public class ObtainDTO {
 
-    @NotBlank(message = "상품명은 필수 입력 값입니다.")
-    private String itemName;        // 상품명
-
-    @NotBlank(message = "수량은 필수 입력 값입니다.")
-    private Long obtainAmount;      // 수량
-
-    @NotBlank(message = "납기일은 필수 입력 값입니다.")
-    private LocalDateTime obtainDate;    // 납기일
+    private Long obtainId;
+    private Long itemId;
+    private Long customerId;
+    private Long obtainAmount;
+    private LocalDateTime obtainDate;
+    private LocalDateTime customerRequestDate;
+    private LocalDateTime expectDate;
+    private boolean obtainStat;
+    private LocalDateTime obtainStatDate;
 
 }
