2a20a773f3ac7f2fce4a3358d8e61fc73f74ff17
Tae0vv <101691583+Tae0vv@users.noreply.github.com>
2023-05-24 17:36:38 +0900

    obtainRepo

--- src/main/java/team_2p4p/mes/repository/ObtainRepository.java
+++ src/main/java/team_2p4p/mes/repository/ObtainRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import team_2p4p.mes.entity.Obtain;
 
+import java.util.List;
+
 public interface ObtainRepository extends JpaRepository<Obtain,Long>,
         QuerydslPredicateExecutor<Obtain>, ObtainRepositoryCustom {
+    List<Obtain> findByObtainStat(boolean bool);
 
 }
