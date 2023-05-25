08ee5df654e6898d9a0a6af36a1721e41c193c39
HalroWorld <halro92@gmail.com>
2023-05-25 13:16:48 +0900

    수주 등록 테스트

--- src/main/java/team_2p4p/mes/controller/ObtainController.java
+++ src/main/java/team_2p4p/mes/controller/ObtainController.java
@@ -1,37 +1,42 @@
-//package team_2p4p.mes.controller;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.PageRequest;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import team_2p4p.mes.entity.Process;
-//import team_2p4p.mes.entity.*;
-//import team_2p4p.mes.repository.*;
-//import team_2p4p.mes.service.ObtainService;
-//
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@RestController
-//@RequestMapping("juicyfresh/")
-//@RequiredArgsConstructor
-//public class ObtainController {
-//    private final ObtainService obtainService;
-//
-//    private final ObtainRepository obtainRepository;
-//
-//    @PostMapping(value = "obtain/add")
-//    public List obtainAdd(){
-//        List<Obtain> obtainList = obtainRepository.findAll();
-//        return obtainList;
-//    }
-//    @GetMapping(value = "obtain/list")
-//    public List<Obtain> obtains(){
-//        List<Obtain> obtainList = obtainRepository.findAll();
-//        return obtainList;
-//    }
-//}
+package team_2p4p.mes.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import team_2p4p.mes.dto.ObtainDTO;
+import team_2p4p.mes.entity.Process;
+import team_2p4p.mes.entity.*;
+import team_2p4p.mes.repository.*;
+import team_2p4p.mes.service.ObtainService;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("juicyfresh/")
+@RequiredArgsConstructor
+public class ObtainController {
+    private final ObtainService obtainService;
+
+    private final ObtainRepository obtainRepository;
+
+    @PostMapping("/obtain/add")
+    public void obtainAdd(@RequestBody ObtainDTO obtainDTO, Model model) {
+        try {
+            System.out.println(obtainDTO);
+
+        } catch (IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+        }
+
+    }
+    @GetMapping("/obtain/list")
+    public List<Obtain> obtains(){
+        List<Obtain> obtainList = obtainRepository.findAll();
+        return obtainList;
+    }
+}
--- src/main/java/team_2p4p/mes/dto/ObtainDTO.java
+++ src/main/java/team_2p4p/mes/dto/ObtainDTO.java
@@ -7,6 +7,7 @@ import team_2p4p.mes.entity.Customer;
 import team_2p4p.mes.entity.Item;
 import team_2p4p.mes.entity.Obtain;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Getter
@@ -21,21 +22,24 @@ public class ObtainDTO {
 
     private Item item;
 
+    @NotBlank(message = "제품 이름은 필수 입력 값입니다.")
     //제품 이름
     private String itemName;
-
 
 
     //고객 번호
     private Long customerId;
 
     private Customer customer;
+
+    @NotBlank(message = "수량은 필수 입력 값입니다.")
     //수량
     private Long obtainAmount;
 
     //수주일
     private LocalDateTime obtainDate;
 
+    @NotBlank(message = "납기일은 필수 입력 값입니다.")
     //납기일
     private LocalDateTime customerRequestDate;
 
